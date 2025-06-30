c4e461c65f8548fb8e6d6f96c8326d0fa76266a5
Hristo Hristoff <hristo.hristoff@eurekasoft.eu>
2025-06-30 14:50:58 +0300

    WIP on fix-cutom-formula-in-formula

--- lib/src/parse.dart
+++ lib/src/parse.dart
@@ -483,8 +483,7 @@ MathNode _parseMathString(
       if (r.isNotEmpty) nodes.add(_MathNodePartString(r));
     } else {
       if (item.contents == '') continue;
-
-      final sp = item.contents.split(',');
+      final sp = item.contents.split(r',\s*(?![^()]*\))');
       final List<MathNode> n = [];
 
       for (final r in sp) {
